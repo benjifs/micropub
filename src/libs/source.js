d4f335006e6dcc725f573ed7fe11992aaebadfa7
Benji Encalada Mora <benjifs@users.noreply.github.com>
2021-10-07 02:11:32 -0500

    feat: handle additional GET queries

---
+++ src/libs/source.js
@@ -0,0 +1,45 @@
+
+import GitHub from './github'
+import content from './content'
+import { utils } from './utils'
+
+const parsedToSource = (parsed, properties) => {
+	const source = {
+		'type': parsed.type,
+		'properties': {
+			'published': [parsed.date],
+			'content': [parsed.content],
+			'category': parsed.tags,
+			'title': parsed.title
+		}
+	}
+	if (properties) {
+		delete source.type
+		for (let key in source.properties) {
+			if (!properties.includes(key)) {
+				delete source.properties[key]
+			}
+		}
+	}
+	return source
+}
+
+export default {
+	get: async (url, properties) => {
+		const filename = utils.urlToFilename(url)
+		if (!filename) {
+			return { 'error': 'invalid url' }
+		}
+		const exists = await GitHub.getFile(filename)
+		if (!exists) {
+			return { 'error': 'file does not exist' }
+		}
+		const parsed = content.parse(exists.content)
+		if (!parsed) {
+			return { 'error': 'could not parse file' }
+		}
+		return {
+			'source': parsedToSource(parsed, properties)
+		}
+	}
+}
--- src/micropub.js
+++ src/micropub.js
@@ -9,17 +9,35 @@ import jsonBodyParser from '@middy/http-json-body-parser'
 dotenv.config()
 
 import auth from './libs/auth'
+import source from './libs/source'
 import publish from './libs/publish'
 import { Error, Response } from './libs/response'
+// q=source&properties%5B%5D=content&properties%5B%5D=category&url=https%3A%2F%2Fbenji.dog%2Fnotes%2F1632514908
+const getHandler = async query => {
+	let res
+	if (query.q === 'config') {
+		return Response.send(200, {
+			'media-endpoint': process.env.MEDIA_ENDPOINT || `${process.env.URL}/.netlify/functions/media`,
+			'syndicate-to': []
+		})
+	} else if (query.q === 'source' && query.url) {
+		res = await source.get(query.url, query.properties || query['properties[]'])
+		if (res && res.source) {
+			return Response.send(200, res.source)
+		}
+	} else if (query.q === 'syndicate-to') {
+		return Response.send(200, {
+			'syndicate-to': []
+		})
+	}
+	return Response.error(Error.INVALID, res && res.error)
+}
 
 const micropubFn = async event => {
+	if (event.httpMethod == 'GET') {
+		return getHandler(event.queryStringParameters)
+	}
 	if (event.httpMethod !== 'POST') {
-		const query = event.queryStringParameters
-		if (query && query.q === 'config') {
-			return Response.send(200, {
-				'media-endpoint': process.env.MEDIA_ENDPOINT || `${process.env.URL}/.netlify/functions/media`
-			})
-		}
 		return Response.error(Error.NOT_ALLOWED)
 	}
 
